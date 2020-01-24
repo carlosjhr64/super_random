30b8175c1a0f2d1f54d949f50019ef4b8883c182
carlosjhr64 <carlosjhr64@gmail.com>
2020-01-23 16:12:50 -0800

    aliased #random_number to #rand, reviewed/rewrite #rand.

--- lib/super_random/super_random.rb
+++ lib/super_random/super_random.rb
@@ -1,4 +1,5 @@
 class SuperRandom
+  DEFAULT_BYTES = 32
 
   # http://qrng.anu.edu.au/index.php
   # https://qrng.anu.edu.au/API/api-demo.php
@@ -77,7 +78,7 @@ class SuperRandom
     @services = 0
   end
 
-  def bytes(n=32)
+  def bytes(n=DEFAULT_BYTES)
     @randomness = 0.0
     @services = 0
 
@@ -122,13 +123,43 @@ class SuperRandom
     return a
   end
 
-  def hexadecimal(n=32)
+  def hexadecimal(n=DEFAULT_BYTES)
     bytes(n).map{|i|i.to_s(16).rjust(2,'0')}.join
   end
 
-  def rand(m=nil, n=6)
-    div = n.times.inject(''){|s,i| s+'FF'}.to_i(16).to_f
-    r = hexadecimal(n).to_i(16).to_f / div
-    m.nil? ? r : (m*r).to_i
+  def rand(scale=1.0, minbytes=6, maxbytes=[minbytes,DEFAULT_BYTES].max)
+    case scale
+    when Float
+      div = minbytes.times.inject(''){|s,i| s+'FF'}.to_i(16)
+      den = hexadecimal(minbytes).to_i(16)
+      return  scale * den.to_f / div.to_f
+    when Integer
+      n = n0 = Math.log(scale, 256).ceil
+      e = e0 = 256**n
+      r = r0 = e0 % scale
+      while r > 0
+        n0 += 1
+        e0 *= 256
+        r0 = e0 % scale
+        if r0 <= r
+          # break if repeating pattern with big enough integer
+          break if r0 == r and n0 > minbytes
+          r,n,e = r0,n0,e0
+        end
+        break if n0 >= maxbytes
+      end
+      max = (e/scale)*scale
+      loop do
+        number = hexadecimal(n).to_i(16)
+        return number % scale if number < max
+        # On a relatively small chance that we're above max...
+        if @nevermind
+          warn "using SecureRandom.random_number(#{scale})"
+          return SecureRandom.random_number(scale)
+        end
+      end
+    end
+    raise "rand(scale Integer|Float)"
   end
+  alias random_number rand
 end
