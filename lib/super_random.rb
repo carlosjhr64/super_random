242ec41fe53b1f98e960ac3c1262599c8d36bbfd
CarlosJHR64 <carlosjhr64@gmail.com>
2023-01-10 12:32:30 -0800

    New way for 3.0

--- lib/super_random.rb
+++ lib/super_random.rb
@@ -1,15 +1,88 @@
-# Standard Libraries
 require 'timeout'
 require 'securerandom'
-require 'net/http'
-require 'json'
+require 'open-uri'
+require 'digest'
+# Requires:
+#`ruby`
 
 class SuperRandom
   VERSION = '3.0.230110'
-  # This Gem
-  require 'super_random/services'
-  require 'super_random/generator'
-end
+  DEFAULT_SOURCES = [
+    'https://news.google.com',
+    'https://news.yahoo.com',
+    'https://nytimes.com',
+  ]
+  SHASUM = Digest::SHA2.new(512)
+  MUTEX = Mutex.new
+  DIV = 128.times.inject(''){|h,_|h<<'f'}.to_i(16)
+  RATE_LIMIT = 60 # One minute, be nice!
+  @@LAST_TIME = Time.now.to_i - RATE_LIMIT
 
-# Requires:
-#`ruby`
+  attr_accessor :first_timeout, :second_timeout, :nevermind
+  attr_reader   :sources, :source_count, :byte_count
+  def initialize(*sources)
+    @sources = sources.empty? ? DEFAULT_SOURCES.dup : sources
+    @byte_count,@source_count = 0,0
+    @first_timeout,@second_timeout,@nevermind = 3,6,true
+  end
+
+  # Returns 64 random bytes(at least from SecureRandom's)
+  def bytes
+    @byte_count,@source_count = 0,0
+    _do_updates if Time.now.to_i - @@LAST_TIME > RATE_LIMIT
+    SHASUM.digest.chars.map{(_1.ord + SecureRandom.random_number(256))%256}
+  end
+
+  def hexadecimal
+    bytes.map{_1.to_s(16).rjust(2,'0')}.join
+  end
+
+  def random_number(scale=1.0)
+    case scale
+    when Float
+      return scale * Rational(hexadecimal.to_i(16), DIV)
+    when Integer
+      return ((hexadecimal.to_i(16) + SecureRandom.random_number(scale)) % scale)
+    end
+    raise "rand(scale Integer|Float)"
+  end
+  alias rand random_number
+
+  private
+
+  def _update_with(source)
+    URI.open(source) do |tmp|
+      MUTEX.synchronize do
+        tmp.each_line do |line|
+          SHASUM.update line
+          @byte_count += line.bytesize
+        end
+        @source_count += 1
+      end
+    end
+  rescue
+    $stderr.puts $!
+  end
+
+  def _do_updates
+    @@LAST_TIME = Time.now.to_i
+    threads = @sources.inject([]){|t,s|t.push Thread.new{_update_with s}}
+    begin
+      # Initially, would like to get them all.
+      Timeout.timeout(@first_timeout){threads.each{_1.join}}
+    rescue Timeout::Error
+      begin
+        Timeout.timeout(@second_timeout) do
+          # But at this point,
+          # would like to get at least one.
+          while @services<1 and threads.any?{_1.alive?}
+            Thread.pass
+          end
+        end
+      rescue Timeout::Error
+        Mutex.sychronize{threads.each{_1.exit}} # Exit each thread
+        raise $! unless @nevermind # Go on if never-minding
+      end
+    end
+  end
+end
